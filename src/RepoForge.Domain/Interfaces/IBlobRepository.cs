83a4063a3827ccef3043d0d42b029d5634cbfd98
Yuri A. Nascimento <yurinascimento@outlook.com>
2025-10-01 10:33:22 -0300

    feat: Ajuste de pastas

--- src/RepoForge.Domain/Interfaces/IBlobRepository.cs
+++
@@ -1,8 +0,0 @@
-namespace RepoForge.Domain.Interfaces;
-
-public interface IBlobRepository
-{
-    Task UploadAsync(string key, Stream data);
-    Task<Stream?> DownloadAsync(string key);
-    Task DeleteAsync(string key);
-}
